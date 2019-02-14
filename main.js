db837f40620912e7004b7d9b5628c0e87f277de7
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-02-14 16:29:36 +0530

    +Wave bg-blog section
    +10 line sinusoidal funstion based wave curve animation

--- main.js
+++ main.js
@@ -1,3 +1,63 @@
+// Blog Hero section
+// Wave Lines
+//motivation
+//https://codepen.io/DirkTester/pen/BJLgEx?editors=1010
+
+var c = document.getElementById("canv");
+var $ = c.getContext("2d");
+
+var w = (c.width = window.innerWidth);
+var h = (c.height = window.innerHeight);
+
+var intLines = 10;
+var draw = function (a, b, t) {
+  $.lineWidth = 0.2;
+  //background color
+  $.fillStyle = "white";
+  $.fillRect(0, 0, w, h);
+  //i sets the number of lines
+  for (var i = 0; i < intLines; i++) {
+    $.strokeStyle = "#B1B1B1";
+    $.beginPath();
+    $.moveTo(-0, h / 2);
+    for (var j = 0; j < w; j += 10) {
+      $.lineTo(
+        60 * Math.sin(i / 7) + j + 0.008 * j * j,
+        Math.floor(
+          h / 1.9 +
+          j / 2 * Math.sin(j / 50 - t / 50 - i / 118) +
+          i * 0.9 * Math.sin(j / 25 - (i + t) / 65)
+        )
+      );
+    }
+    $.stroke();
+  }
+};
+
+var t = 0;
+var run = function () {
+  //note the self-calling function via callback window.requestAnimationFrame
+  //window.requestAnimationFrame(run);
+  t += 0.5;
+  draw(33, 52 * Math.sin(t / 2400), t);
+  txt();
+};
+
+//basic UI
+function changeLines() {
+  var L = document.getElementById("lineCount").value;
+  intLines = L;
+  //console.log("test");
+}
+document.getElementById("lineCount").value = intLines;
+document.getElementById("lineCount").onchange = function () {
+  changeLines();
+};
+
+window.setInterval(run, 50); //interval instead of using callback in run() function
+//run();
+
+
 // smooth scroll
 // $("html").easeScroll();
 ///aos scrolling animation
